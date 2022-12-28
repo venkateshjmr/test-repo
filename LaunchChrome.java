668d5891ea051607319f2819099051f7c5556dec
venkateshjmr <117890461+venkateshjmr@users.noreply.github.com>
2022-12-28 17:19:50 +0530

    Update LaunchChrome.java

--- LaunchChrome.java
+++ LaunchChrome.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LaunchChrome {
 	public static void main(String[] args) {
-		System.setProperty("webdriver.chrome.driver", "D:/JMR/Software/chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "/tmp/chromedriver");
 		
 		WebDriver driver = new ChromeDriver();
 		driver.get("http://www.google.com");
